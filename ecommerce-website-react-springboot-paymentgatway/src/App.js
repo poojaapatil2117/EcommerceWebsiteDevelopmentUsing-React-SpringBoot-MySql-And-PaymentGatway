e4c1190b62e43b09e2350984da5f2dc66d582c05
poojaapatil2117 <poojalpatil21@gmail.com>
2025-05-01 08:58:25 +0530

    Update 30/5

---
+++ ecommerce-website-react-springboot-paymentgatway/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/Pages/HomePage/HomePage';
+import Footer from './customer/components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        <HomePage/>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
