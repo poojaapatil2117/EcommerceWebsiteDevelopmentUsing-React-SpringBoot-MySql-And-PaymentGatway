fd420bbb7f11682e2d27b7887221307b1487aee7
poojaapatil2117 <poojalpatil21@gmail.com>
2025-05-10 19:43:50 +0530

    Done With Front-End-Work

--- ecommerce-website-react-springboot-paymentgatway/src/customer/components/Cart/Cart.jsx
+++ ecommerce-website-react-springboot-paymentgatway/src/customer/components/Cart/Cart.jsx
@@ -1,60 +1,81 @@
 import React from "react";
-import CartItems from "./CartItems";
-import { Button, Divider } from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import CartItem from "./CartItem";
+import { Badge, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getCart } from "../../../Redux/Customers/Cart/Action";
+
 const Cart = () => {
-
-
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleCheckOut = ()=>{
-    navigate("/checkout?step=2");
-  }
+  const jwt = localStorage.getItem("jwt");
+  const {cart}=useSelector(store=>store);
+  console.log("cart ",cart)
 
+  useEffect(() => {
+    dispatch(getCart(jwt));
+  }, [jwt]);
+  
   return (
-    <div>
-      <div className="lg:grid grid-cols-3 lg:px-16 relative ">
-        <div className="col-span-2">
-          {[1, 1, 1, 1].map((iteml) => (
-            <CartItems />
+    <div className="">
+      {cart.cartItems?.length>0 ? <div className="lg:grid grid-cols-3 lg:px-16 relative">
+        <div className="lg:col-span-2 lg:px-5 bg-white">
+        <div className=" space-y-3">
+          {cart.cartItems.map((item) => (
+            <>
+              <CartItem item={item} showButton={true}/>
+            </>
           ))}
         </div>
+      </div>
+      <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
+        <div className="border p-5 bg-white shadow-lg rounded-md">
+          <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
+          <hr />
 
-        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
-          <div className="border">
-            <p className="uppercase font-bold opacity-60 pb-4 p-3">
-              Price Details
-            </p>
-            <hr />
-            {/* <Divider/> */}
-            <div className="space-y-3 font-semibold mb-10 p-4">
-              <div className="flex justify-between pt-3 text-black">
-                <span>Price</span>
-                <span className="text-green-600">₹4675</span>
-              </div>
-              <div className="flex justify-between pt-3 text-black">
-                <span>Discount</span>
-                <span className="text-green-600">-₹3419</span>
-              </div>
-              <div className="flex justify-between pt-3 text-black">
-                <span>Dilevery Charge</span>
-                <span className="text-green-600">Free</span>
-              </div>
-              <div className="flex justify-between pt-3 text-black font-bold">
-                <span>Total Amount</span>
-                <span className="text-green-600 font-bold">₹4175</span>
-              </div>
+          <div className="space-y-3 font-semibold">
+            <div className="flex justify-between pt-3 text-black ">
+              <span>Price ({cart.cart?.totalItem} item)</span>
+              <span>₹{cart.cart.totalPrice}</span>
             </div>
-
-            <Button onClick={handleCheckOut}
-              className="w-full mt-4 p-3"
-              variant="contained"
-              sx={{ px: "2.5rem", py: ".7rem", bgcolor: "#9155fd" }}
-            >
-              Check Out
-            </Button>
+            <div className="flex justify-between">
+              <span>Discount</span>
+              <span className="text-green-700">-₹{cart.cart?.discounte}</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Delivery Charges</span>
+              <span className="text-green-700">Free</span>
+            </div>
+            <hr />
+            <div className="flex justify-between font-bold text-lg">
+              <span>Total Amount</span>
+              <span className="text-green-700">₹{cart.cart?.totalDiscountedPrice}</span>
+            </div>
           </div>
+
+          <Button
+            onClick={() => navigate("/checkout?step=2")}
+            variant="contained"
+            type="submit"
+            sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%" }}
+          >
+            Check Out
+          </Button>
         </div>
       </div>
+      </div>: <div className="h-[85vh] flex justify-center items-center flex-col">
+          <div className="text-center py-5">
+            <h1 className="text-lg font-medium">hay its feels so light!</h1>
+            <p className="text-gray-500 text-sm">
+              there is nothing in your bag, lets add some items
+            </p>
+          </div>
+          <Button onClick={()=>navigate("/")} variant="outlined" sx={{ py: "11px" }}>
+            Add Item From Wishlist
+          </Button>
+        </div>}
+      
     </div>
   );
 };
--- ecommerce-website-react-springboot-paymentgatway/src/customer/components/Checkout/Checkout.jsx
+++ ecommerce-website-react-springboot-paymentgatway/src/customer/components/Checkout/Checkout.jsx
@@ -1,79 +1,59 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useLocation } from 'react-router-dom';
-import DeliveryAddressForm from './DeliveryAddressForm';
-import OrderSummary from './OrderSummary';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import AddDeliveryAddressForm from "./AddAddress";
+import { useLocation, useNavigate } from "react-router-dom";
+import OrderSummary from "./OrderSummary";
 
-const steps = ['Log in', 'Delevery Address', 'Order Summary','Payment'];
+const steps = [
+  "Login",
+  "Delivery Adress",
+  "Order Summary",
+  "Payment",
+];
 
-export default function HorizontalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
+export default function Checkout() {
+  const [activeStep, setActiveStep] = React.useState(1);
+  const [skipped, setSkipped] = React.useState(new Set());
   const location = useLocation();
-  const querySearch = new URLSearchParams(location.search);
-  const step = querySearch.get("step")
+  const queryParams = new URLSearchParams(location.search);
+  const step = queryParams.get('step');
+  const navigate=useNavigate();
+ 
+console.log("step",step)
 
-//   const isStepOptional = (step) => {
-//     return step === 1;
-//   };
-
-//   const isStepSkipped = (step) => {
-//     return skipped.has(step);
-//   };
 
   const handleNext = () => {
-    // let newSkipped = skipped;
-    // if (isStepSkipped(activeStep)) {
-    //   newSkipped = new Set(newSkipped.values());
-    //   newSkipped.delete(activeStep);
-    // }
+    let newSkipped = skipped;
+   
 
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    // setSkipped(newSkipped);
+    setSkipped(newSkipped);
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    navigate(`/checkout?step=${step-1}`)
   };
 
-//   const handleSkip = () => {
-//     if (!isStepOptional(activeStep)) {
-//       // You probably want to guard against something like this,
-//       // it should never occur unless someone's actively trying to break something.
-//       throw new Error("You can't skip a step that isn't optional.");
-//     }
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
-//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-//     setSkipped((prevSkipped) => {
-//       const newSkipped = new Set(prevSkipped.values());
-//       newSkipped.add(activeStep);
-//       return newSkipped;
-//     });
-//   };
-
-//   const handleReset = () => {
-//     setActiveStep(0);
-//   };
+  const handlePayment=()=>{
+    console.log("handle payment")
+  }
 
   return (
-    <div className='px-10 lg:px-20 '>
-        <Box sx={{ width: '100%' }}>
+    <Box className="px-5 lg:px-32 " sx={{ width: "100%" }}>
       <Stepper activeStep={step}>
         {steps.map((label, index) => {
           const stepProps = {};
           const labelProps = {};
-        //   if (isStepOptional(index)) {
-        //     labelProps.optional = (
-        //       <Typography variant="caption">Optional</Typography>
-        //     );
-        //   }
-        //   if (isStepSkipped(index)) {
-        //     stepProps.completed = false;
-        //   }
+         
           return (
             <Step key={label} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
@@ -86,43 +66,37 @@ export default function HorizontalLinearStepper() {
           <Typography sx={{ mt: 2, mb: 1 }}>
             All steps completed - you&apos;re finished
           </Typography>
-          {/* <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Box sx={{ flex: "1 1 auto" }} />
             <Button onClick={handleReset}>Reset</Button>
-          </Box> */}
+          </Box>
         </React.Fragment>
       ) : (
         <React.Fragment>
-
-
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
             <Button
               color="inherit"
-              disabled={activeStep === 0}
+              disabled={step == 2}
               onClick={handleBack}
               sx={{ mr: 1 }}
             >
               Back
             </Button>
-            {/* <Box sx={{ flex: ' 1 1 auto' }} /> */}
-            {/* {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )} */}
-            {/* <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button> */}
+            <Box sx={{ flex: "1 1 auto" }} />
+
+            
           </Box>
+          {/* <Typography sx={{ my: 6 }}>Title</Typography> */}
 
-          <div className='mt-10'>
-            {step==2 ? <DeliveryAddressForm/>:<OrderSummary/>}
-
+          <div className="my-5">
+            {step == 2? <AddDeliveryAddressForm handleNext={handleNext} />:<OrderSummary/>}
           </div>
+
+          {/* <AddDeliveryAddressForm handleNext={handleNext} /> */}
+
+          
         </React.Fragment>
       )}
     </Box>
-    </div>
-    
   );
 }
