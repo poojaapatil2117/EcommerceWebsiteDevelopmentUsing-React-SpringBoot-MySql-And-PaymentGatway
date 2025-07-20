c2d0354c49a8a9c6ed5da8b84c033081881a9866
poojaapatil2117 <poojalpatil21@gmail.com>
2025-07-20 10:29:04 +0530

    Add Readme.md File

--- ecommerce-website-react-springboot-paymentgatway/src/Pages/Homepage.jsx
+++ ecommerce-website-react-springboot-paymentgatway/src/Pages/Homepage.jsx
@@ -8,7 +8,8 @@ import { gounsPage1 } from "../Data/Gouns/gouns";
 import { kurtaPage1 } from "../Data/Kurta/kurta";
 import { mensShoesPage1 } from "../Data/shoes";
 import { mens_kurta } from "../Data/Men/men_kurta";
-import { lengha_page1 } from "../Data/Women/LenghaCholi";
+// import { lehngacholiPage2 } from "../Data/Women/LenghaCholi";
+import { lehngacholiPage2 } from "../Data/Saree/lenghaCholiPage2";
 
 const Homepage = () => {
 
@@ -19,7 +20,7 @@ const Homepage = () => {
       <div className="space-y-10 py-20">
       <HomeProductSection data={mens_kurta} section={"Men's Kurta"} />
         <HomeProductSection data={mensShoesPage1} section={"Men's Shoes"} />
-        <HomeProductSection data={lengha_page1} section={"Lengha Choli"} />
+        <HomeProductSection data={lehngacholiPage2} section={"Lengha Choli"} />
         <HomeProductSection data={sareePage1} section={"Saree"} />
         <HomeProductSection data={dressPage1} section={"Dress"} />
         <HomeProductSection data={gounsPage1} section={"Women's Gouns"} />
--- ecommerce-website-react-springboot-paymentgatway/src/customer/components/Carousel/HomeCaroselData.js
+++ ecommerce-website-react-springboot-paymentgatway/src/customer/components/Carousel/HomeCaroselData.js
@@ -1,20 +1,19 @@
 export const homeCarouselData=[
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/SALE60.jpg?v=1751439933&width=2000",
         path:"/women/clothing/lengha_choli"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/9_c63833dd-f002-4f46-8f03-9e7a58a114e5.jpg?v=1751396961&width=2000",
         path:"/women/clothing/women_dress"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/17_bridal_lehenga_choli_1920x.jpg?v=1750934273g",
         path:"/women/clothing/women_dress"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
+        image:" https://www.ethnicplus.in/cdn/shop/files/7.jpg?v=1751394501&width=2000",
         path:"/women/clothing/women_saree"
     }
-
 ]
 
